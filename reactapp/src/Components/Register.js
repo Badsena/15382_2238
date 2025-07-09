e344fab22ec5dee57d35573738a1ad2ab28afb19
Badsena <sena@amypo.in>
2025-07-09 00:33:09 +0000

    User

--- reactapp/src/Components/Register.js
+++ reactapp/src/Components/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import axios from "axios";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Register() {
   const [form, setForm] = useState({
@@ -81,7 +81,7 @@ function Register() {
               >
                 Register
               </button>
-              <Link to="/">Login</Link>
+              {/* <Link to="/">Login</Link> */}
             </div>
           </div>
         </div>
